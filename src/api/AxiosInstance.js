1848fd5d84bb581749a3b1b1fadcfd5a9b0eee68
ehsilver98@gmail.com <ehsilver98@gmail.com>
2024-07-02 17:36:10 +0900

    할인율, 토큰 수정

--- src/api/AxiosInstance.js
+++ src/api/AxiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getCookie, setCookie } from "../util/Cookies.js";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "http://localhost:8080/api",
   withCredentials: true,
 });
 
